0bafd0c32a9848f202b8ccf77ad4bfb374ae1999
K MOHITH KANNAN <promohith.535@email.com>
2026-03-11 13:40:27 +0530

    UC2: Display Hello with command-line argument

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -2,7 +2,9 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        }
 
     }
 
